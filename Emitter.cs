abff715dbb9d8c40fc0f459d9ffedbb928b868f1
AlexanderM01 <91949114+AlexanderM01@users.noreply.github.com>
2022-01-20 16:10:18 +0800

    добавил абстрактный класс IImpactPiont

--- Emitter.cs
+++ Emitter.cs
@@ -46,7 +46,7 @@ namespace WindowsFormsApp2
                     {
                         float gX = point.X - particle.X;
                         float gY = point.Y - particle.Y;
-                        float r2 = gX * gX + gY * gY;
+                        float r2 = (float)Math.Max(100, gX * gX + gY * gY);
                         float M = 100;
 
                         particle.SpeedX += (gX) * M / r2;
