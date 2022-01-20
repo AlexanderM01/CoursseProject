68f72c2dd16e8b7e3063ac49668f2ddcf3b3ab97
AlexanderM01 <91949114+AlexanderM01@users.noreply.github.com>
2022-01-20 15:53:34 +0800

    добавил класс Emitter

---
+++ Emitter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp2
+{
+    class Emitter
+    {
+        List<Particle> particles = new List<Particle>();
+        public int MousePositionX;
+        public int MousePositionY;
+
+        public void UpdateState()
+        {
+            /* АНТИ-КОПИПАСТА, устал я код копипасть,
+               так что давайте сами копируйте из своего кода,
+               то что было на форме в методе UpdateState :Р */
+        }
+
+        public void Render(Graphics g)
+        {
+            // ну тут так и быть уж сам впишу...
+            // это то же самое что на форме в методе Render
+            foreach (var particle in particles)
+            {
+                particle.Draw(g);
+            }
+        }
+    }
+}
