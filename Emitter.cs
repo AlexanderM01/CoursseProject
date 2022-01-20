df2a9fa3056e75e9d25cc682b15e38de087eeb9f
AlexanderM01 <91949114+AlexanderM01@users.noreply.github.com>
2022-01-20 16:51:55 +0800

    вывел параметры генерации частиц в эмиттер

--- Emitter.cs
+++ Emitter.cs
@@ -15,6 +15,19 @@ namespace WindowsFormsApp2
         public int MousePositionX;
         public int MousePositionY;
         public List<IImpactPoint> impactPoints = new List<IImpactPoint>();
+        public int X; // координата X центра эмиттера, будем ее использовать вместо MousePositionX
+        public int Y; // соответствующая координата Y 
+        public int Direction = 0; // вектор направления в градусах куда сыпет эмиттер
+        public int Spreading = 360; // разброс частиц относительно Direction
+        public int SpeedMin = 1; // начальная минимальная скорость движения частицы
+        public int SpeedMax = 10; // начальная максимальная скорость движения частицы
+        public int RadiusMin = 2; // минимальный радиус частицы
+        public int RadiusMax = 10; // максимальный радиус частицы
+        public int LifeMin = 20; // минимальное время жизни частицы
+        public int LifeMax = 100; // максимальное время жизни частицы
+
+        public Color ColorFrom = Color.White; // начальный цвет частицы
+        public Color ColorTo = Color.FromArgb(0, Color.Black); // конечный цвет частиц
 
         public void UpdateState()
         {
@@ -42,7 +55,7 @@ namespace WindowsFormsApp2
 
             for (var i = 0; i < 10; ++i)
             {
-                if (particles.Count < 500)
+                if (particles.Count < ParticlesCount)
                 {
                     /* ну и тут чуток подкрутили */
                     var particle = new ParticleColorful();
@@ -59,6 +72,7 @@ namespace WindowsFormsApp2
                 }
             }
         }
+        public int ParticlesCount = 500;
         public virtual void ResetParticle(Particle particle)
         {
             particle.Life = 20 + Particle.rand.Next(100);
